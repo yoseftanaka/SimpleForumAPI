bbe443a4ebfcbd29f682b679697fca8ab2ebf29e
yosef.tanaka <yosef.tanaka@gdn-commerce.com>
2021-02-04 11:15:17 +0700

    aaa

---
+++ src/main/java/com/bjora/Bjora/controller/BasicController.java
@@ -0,0 +1,12 @@
+package com.bjora.Bjora.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class BasicController {
+    public String testFunction() {
+        return "hello";
+    }
+}
