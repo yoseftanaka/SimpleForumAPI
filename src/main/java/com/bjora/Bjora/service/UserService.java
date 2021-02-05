4da01dc678c547f08c59c10a2869722884e27326
yosef.tanaka <yosef.tanaka@gdn-commerce.com>
2021-02-05 08:51:47 +0700

    a

--- src/main/java/com/bjora/Bjora/service/UserService.java
+++ src/main/java/com/bjora/Bjora/service/UserService.java
@@ -2,13 +2,14 @@ package com.bjora.Bjora.service;
 
 import com.bjora.Bjora.DTO.userDTO.GetUserDTO;
 import com.bjora.Bjora.DTO.userDTO.PostUserDTO;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface UserService {
     public GetUserDTO getSingleUser(int id);
     public List<GetUserDTO> getAllUser();
-    public GetUserDTO createUser(PostUserDTO postUserDTO);
-    public GetUserDTO updateUser(int userId, PostUserDTO postUserDTO);
+    public GetUserDTO createUser(PostUserDTO postUserDTO, MultipartFile multipartFile);
+    public GetUserDTO updateUser(int userId, PostUserDTO postUserDTO, MultipartFile multipartFile);
     public GetUserDTO deleteUser(int userId);
 }
